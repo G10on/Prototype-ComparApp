c0fd28bc85df8a0c3c668b8491b5a5ce93e51270
hathiramaniaashish <hathiramaniaashish@gmail.com>
2023-05-05 20:34:43 +0100

    Added login page functionality

---
+++ app/src/main/java/com/example/comparapp/utils/Utils.kt
@@ -0,0 +1,13 @@
+package com.example.comparapp.utils
+
+import android.content.Context
+import android.widget.Toast
+
+class Utils {
+
+    fun displayMessage(context: Context, message: String) {
+        val toast = Toast.makeText(context, message, Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+}
