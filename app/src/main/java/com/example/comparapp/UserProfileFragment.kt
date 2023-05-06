e8d5dda07922383d6e1efc81fedcd8e63f8cd4e7
Jiten <jrajesh@inerza.com>
2023-05-06 02:01:30 +0200

    Some fixes

--- app/src/main/java/com/example/comparapp/UserProfileFragment.kt
+++ app/src/main/java/com/example/comparapp/UserProfileFragment.kt
@@ -5,28 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.example.comparapp.databinding.FragmentSearchBinding
+import com.example.comparapp.databinding.FragmentUserProfileBinding
+import com.example.comparapp.viewModel.UserViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [UserProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class UserProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private val viewModel: UserViewModel by viewModels()
+    private lateinit var _binding: FragmentUserProfileBinding
+
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -34,26 +34,63 @@ class UserProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false)
+//        return inflater.inflate(R.layout.fragment_user_profile, container, false)
+        _binding = FragmentUserProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UserProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UserProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        binding = FragmentUserProfileBinding.inflate(inflater)
+
+        binding.btnChangePassword.setOnClickListener { changePassword() }
+        binding.btnSubscribePremium.setOnClickListener { subscribePremium() }
+        binding.btnLogout.setOnClickListener { logOut() }
+    }
+
+    private fun changePassword() {
+        TODO("Not yet implemented")
+    }
+
+    private fun subscribePremium() {
+        viewModel.logout()
+    }
+
+    private fun logOut() {
+        viewModel.logout()
+        findNavController().navigate(R.id.landingPage)
+    }
+
+
+    private fun setUserDocID() {
+        viewModel.user.observe(viewLifecycleOwner) {
+            userDocID = it.userDocID!!
+        }
+    }
+
+
+    private fun getUserData() {
+
+        var imgProfile = view?.findViewById<ImageView>(R.id.img_profile)
+        var txtName = view?.findViewById<TextView>(R.id.txt_name)
+        var txtUsername = view?.findViewById<TextView>(R.id.txt_username)
+        var txtnumFollowers = view?.findViewById<TextView>(R.id.txt_num_followers)
+        var txtnumPoints = view?.findViewById<TextView>(R.id.txt_num_points)
+        var txtnumFollowings = view?.findViewById<TextView>(R.id.txt_num_followings)
+
+        CoroutineScope(Dispatchers.Main).launch{
+            var user = db.getUserByDocID(userDocID!!)
+            if ((user != null) && (imgProfile != null)) {
+                txtUsername!!.text = "@" + user!!.username
+                Glide.with(requireView()).load(user.profile_image).into(imgProfile)
+                txtName!!.text = user.name
+                txtnumFollowings!!.text = user.n_followings.toString()
+                txtnumPoints!!.text = user.n_points.toString()
+                txtnumFollowers!!.text = user.n_followers.toString()
             }
+        }
+
     }
 }
